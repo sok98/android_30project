e921ab5eeb9babc477d4b4d0351e1ce9d8e9a127
SeoYeseul <sok98@naver.com>
2021-08-17 00:39:06 +0900

    UPDATE 12. 도서 리뷰 앱

--- part3/chapter04/app/src/main/java/com/yeseul/part3/chapter04/DetailActivity.kt
+++ part3/chapter04/app/src/main/java/com/yeseul/part3/chapter04/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.yeseul.part3.chapter04
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.bumptech.glide.Glide
@@ -10,7 +11,7 @@ import com.yeseul.part3.chapter04.model.AppDatabase
 import com.yeseul.part3.chapter04.model.Book
 import com.yeseul.part3.chapter04.model.Review
 
-class DetailActivity: AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
     private lateinit var db: AppDatabase
@@ -35,19 +36,24 @@ class DetailActivity: AppCompatActivity() {
 
         Thread {
             val review = db.reviewDao().getOneReview(model?.id?.toInt() ?: 0)
-            runOnUiThread {
-                binding.reviewEditText.setText(review.review.orEmpty())
+            if (review != null) {
+                runOnUiThread {
+                    binding.reviewEditText.setText(review.review.orEmpty())
+                }
             }
+
+
+            Log.d("ERROR", "" + review)
+            Log.d("ERROR", "" + model)
         }.start()
 
         binding.saveButton.setOnClickListener {
             Thread {
                 db.reviewDao().saveReview(
-                    Review(model?.id?.toInt() ?: 0, binding.reviewEditText.text.toString()),
+                    Review(model?.id?.toInt() ?: 0, binding.reviewEditText.text.toString())
                 )
             }.start()
-            Log.d("ReviewSave", "리뷰가 저장되었습니다.")
+            Toast.makeText(this, "리뷰가 저장되었습니다.", Toast.LENGTH_SHORT).show()
         }
     }
-
 }
