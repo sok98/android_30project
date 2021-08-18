29ad4633fb8000dc466f536dcaa9a35805e0cf71
SeoYeseul <sok98@naver.com>
2021-08-18 14:36:20 +0900

    ADD 14. 중고거래 앱

---
+++ part3/chapter06/app/src/main/java/com/yeseul/part3/chapter06/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yeseul.part3.chapter06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
