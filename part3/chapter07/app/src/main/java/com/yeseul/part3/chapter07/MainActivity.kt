bc710630eb485ff679044bd9b6360e6813fda693
SeoYeseul <sok98@naver.com>
2021-08-20 16:33:28 +0900

    15. 에어비앤비 (API 연결 실패)

---
+++ part3/chapter07/app/src/main/java/com/yeseul/part3/chapter07/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.yeseul.part3.chapter07
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
